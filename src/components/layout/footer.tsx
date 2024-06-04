bd3e31b6903b9d8d839136bb1461576be95cd16d
lucianavoica <luciana.voica@stsnet.ro>
2024-06-04 21:44:52 +0300

    footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,7 +2,14 @@ export default function Footer() {
   return (
     <div className="h-[6vh] px-6 border-t bg-card items-center flex">
       <div className={'flex flex-row w-full items-center '}>
-        <div>Mwatza-mea</div>
+        <div className="felx flex-row gap-2">
+          <a
+            target="_blank"
+            href="https://mta.ro/"
+            rel="noreferrer">
+            Academia Tehnica Militara
+          </a>
+        </div>
       </div>
     </div>
   );
